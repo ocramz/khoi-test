5f5df42b3bf5651964945171e2ac5bc7b9781ef5
ocramz <zocca.marco@gmail.com>
2016-10-06 12:16:44 +0200

    first

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,25 @@
 module Main where
 
+
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Distributed.Process
+import Control.Distributed.Process.Node
+
 import Lib (ourAdd)
 
-import Text.Printf (printf)
+-- import Text.Printf (printf)
+
+-- main :: IO ()
+-- main = printf "2 + 3 = %d\n" (ourAdd 2 3)
+
 
 main :: IO ()
-main = printf "2 + 3 = %d\n" (ourAdd 2 3)
+main = do
+  Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+  node <- newLocalNode t initRemoteTable
+  _ <- runProcess node $ do
+    self <- getSelfPid
+    send self "hello"
+    hello <- expect :: Process String
+    liftIO $ putStrLn hello
+  return ()
