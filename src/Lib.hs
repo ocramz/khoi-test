9816700b5936bad69e9658daf7a65bcf0f04cdb2
ocramz <zocca.marco@gmail.com>
2016-10-07 13:28:43 +0200

    adding ser/des in Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,19 @@
--- | A library to do stuff.
+{-# language DeriveDataTypeable, DeriveGeneric #-}
+{-# language TemplateHaskell #-}
 module Lib
-    (
-      ourAdd
-    ) where
+    -- (
+    --   ourAdd
+    -- )
+    where
+
+
+
+import Text.Printf
+import Data.Binary
+import Data.Typeable
+import GHC.Generics (Generic)
+
+
 
 -- | Add two 'Int' values.
 ourAdd :: Int  -- ^ left
